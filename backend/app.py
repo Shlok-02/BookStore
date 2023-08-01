826b42b207b285f60966d000b62a2ac9e938bd3e
shlok.gangatirkar <shlok.gangatirkar@druva.com>
2023-08-01 14:48:26 +0530

    Second Commit

--- backend/app.py
+++ backend/app.py
@@ -1,69 +1,26 @@
-from flask import Flask,jsonify,request
+from cs50 import SQL
+from flask import Flask, jsonify, request
 from http import HTTPStatus
 from book_store import books
 
-app = Flask(__name__)#creates Flask class instance
-if __name__ == '__main__': #starts the flask server
+
+# to initialize connection to DB
+db = SQL("sqlite:///database.db")
+
+# create a tables for books named Books
+
+# db.execute(("create table if not exists books (id INTEGER PRIMARY KEY AUTOINCREMENT,book_title string,book_author string,publisher string,description string)"))
+
+# db.execute("INSERT INTO books(book_title,book_author,publisher,description) VALUES(?,?,?,?)" , "GOD of WAR","Jack Hughman","chowa","war history")
+# db.execute("INSERT INTO books(book_title,book_author,publisher,description) VALUES(?,?,?,?)" , "Splinter cell","Tom clancy","cross","splinting of cells")
+# db.execute("INSERT INTO books(book_title,book_author,publisher,description) VALUES(?,?,?,?)" , "Monsters","jackie bu","meme","are monster real")
+
+
+app = Flask(__name__)  # creates Flask class instance
+if __name__ == '__main__':  # starts the flask server
      app.run(use_reloader=True)
 
-@app.route('/',methods=['GET'])
+
+@app.route('/get', methods=['GET'])
 def get_books():
-     return jsonify({"books_data":books})
-
-@app.route('/create',methods=['POST'])
-def create_book():
-
-     data = request.get_json()
-     for book in books:
-          if book['book_title']==data['book_title']:
-               return jsonify({"Error": "Book already present"})
-
-
-     title=data['book_title']
-     author=data['book_author']
-     publisher=data['publisher']
-     description=data['description']
-     id=len(books)+1
-     newBook={
-          "id":id,
-          "book_title":title,
-          "book_author":author,
-          "publisher":publisher,
-          "description":description
-     }
-
-     books.append(newBook)
-
-     return jsonify({"status code":201})
-
-@app.route('/update',methods=['PUT'])
-def update_book():
-     data=request.get_json()
-
-     for book in books:
-          if book['book_title']==data['book_title']:
-               book.update(
-                    {
-                         'book_title': data.get('book_title'), 'book_author': data.get('book_author'),
-                         'publisher': data.get('publisher'), 'description': data.get('description'),
-                    }
-               )
-               return jsonify(book)
-
-
-@app.route('/delete/<int:book_id>',methods=['DELETE'])
-def delete_book(book_id):
-
-     for i in range(0,len(books)):
-          if books[i].get('id')==book_id:
-               del books[i]
-     return jsonify(books)
-
-@app.route('/book/<int:book_id>',methods=['GET'])
-def get_book(book_id):
-
-     for i in range(0,len(books)):
-          if books[i].get('id')==book_id:
-               return jsonify(books[i])
-
-     return jsonify({"Error":"book not found"})
+     return jsonify({"books_data": books})
